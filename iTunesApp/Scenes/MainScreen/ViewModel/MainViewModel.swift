225d8821943cb745f63ee29aa221e35dcfd9dbe7
Onuralp <onuralp.yahnici@huawei.com>
2022-12-12 16:41:35 +0300

    Add initializers and fetchrequest

--- iTunesApp/Scenes/MainScreen/ViewModel/MainViewModel.swift
+++ iTunesApp/Scenes/MainScreen/ViewModel/MainViewModel.swift
@@ -17,7 +17,44 @@ protocol MainViewModelDelegate: AnyObject {
 }
 final class MainViewModel  {
     weak var delegate: MainViewModelDelegate?
+    let networkManager: NetworkManager<MainEndpointItem>
     private var upComingMediaList: [String] = []
+    
+    enum MainConstants {
+        static let defaultQueryStr: String = "all"
+        static let threadCount: Int = 3
+    }
+    
+    init(networkManager: NetworkManager<MainEndpointItem>) {
+        self.networkManager = networkManager
+    }
+
+
+    
+    
+    private func fetchUpComingMovieList(query: String = MainConstants.defaultQueryStr){
+        networkManager.request(endpoint: .upcoming(query: query), type: MediaResponse.self) { result in
+            let queue = OperationQueue()
+            queue.maxConcurrentOperationCount = MainConstants.threadCount
+            let blockOperation = BlockOperation {
+                switch result {
+                case .success(let response):
+                    let movieList = response.results
+                    let medias = movieList.compactMap{ $0 }
+                    medias.forEach({ media in
+                        media.screenshotUrls.forEach { string in
+                        }
+                    })
+                    break
+                case .failure(let error):
+                    print(String(describing: error))
+                    break
+                }
+            }
+            queue.addOperation(blockOperation)
+        }
+    }
+
 }
 
 extension MainViewModel : MainViewModelProtocol {
