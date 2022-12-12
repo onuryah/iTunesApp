ebe997fdf61b7d6216e5d1f3c9bc39100f05fb06
Onuralp <onuralp.yahnici@huawei.com>
2022-12-12 17:35:01 +0300

    Add collectionViewMethods

--- iTunesApp/Scenes/MainScreen/View/MainView/MainViewController.swift
+++ iTunesApp/Scenes/MainScreen/View/MainView/MainViewController.swift
@@ -36,7 +36,42 @@ class MainViewController: UIViewController, MainViewModelDelegate {
 }
 
 extension MainViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        viewModel.giveTitlesOfSections(view: dataCheckView).count
+    }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        viewModel.giveCombined()[section].count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "UpComingTableViewCell", for: indexPath) as! UpcomingCollectionViewCell
+        viewModel.configureUpComingTableViewCell(cell, forIndexPath: indexPath)
+        return cell
+    }
+    
+    fileprivate func setCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return viewModel.calculateCellHeight(collectionView: collectionView)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedSection = viewModel.giveCombined()[indexPath.section]
+        let selectedItem = selectedSection[indexPath.item]
+        viewModel.pushToDetails(selectedImageString: selectedItem, viewController: self)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let sectionHeaderView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "SectionHeaderView", for: indexPath) as! SectionHeaderView
+        sectionHeaderView.categoryTitle = viewModel.giveTitlesOfSections(view: dataCheckView)[indexPath.section]
+        return sectionHeaderView
+    }
+
 }
 
 extension MainViewController : UISearchResultsUpdating {
